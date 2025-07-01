cca5a422c0e7a2c402c9dd7a23dc4afc2a8108b2
Rohan singh negi <negirohan10ep@gmail.com>
2025-07-01 20:31:10 +0530

    Add files via upload

---
+++ 8.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main(){
+    int a ;
+    printf("Enter a number");
+    scanf("%d",&a);
+
+    // if(a/100 <= 9){
+    //     printf("Three digit");
+    // }
+    // else if(a/100 > 9){
+    //     printf("More than three digit");
+    // }
+    // else{
+    //     printf("Less than three digit");
+    // }
+
+    // int b;
+    // printf("Enter another number");
+    // scanf("%d",&b);
+    
+    // if (a > b )
+    // {
+    //     printf("%d is greater than %d", a,b);
+    // }
+    // else
+    // if (b > a )
+    // {
+    //     printf("%d is greater than %d", b,a);
+    // }
+    // else{
+    //     printf("Both have %d is same",a);
+    // }
+
+    // int c ;
+    // printf("Enter the constant");
+    // scanf("%d",&c);    
+    // int ans = (a*a)-(4*a*c);
+    // if (ans = 0 )
+    // {
+    //     printf("Same and real roots");
+    // }
+    // else
+    // if (ans > 1 )
+    // {
+    //     printf("Distinct and real roots");
+    // }
+    // else
+    // {
+    //     printf("Imaginary roots");
+    // }
+    
+
+    
+
+
+}
