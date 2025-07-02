770489b1d40fb982f5cb2f6a03c3ed74bcec2e37
Rohan singh negi <negirohan10ep@gmail.com>
2025-07-02 18:47:43 +0530

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(){
+//    float cost_price, selling_price, profit_loss, percentage;
+
+//     printf("Enter cost price: ");
+//     scanf("%f", &cost_price);
+
+//     printf("Enter selling price: ");
+//     scanf("%f", &selling_price);
+
+//     profit_loss = selling_price - cost_price;
+
+//     if(profit_loss > 0) {
+//         percentage = (profit_loss / cost_price) * 100;
+//         printf("Profit Percentage: %.2f%%\n", percentage);
+//     } else if(profit_loss < 0) {
+//         percentage = (-profit_loss / cost_price) * 100;
+//         printf("Loss Percentage: %.2f%%\n", percentage);
+//     } else {
+//         printf("No Profit No Loss.\n");
+//     }
+
+// int a,b,c,d,e;
+// printf("Write the marks of your subject out of 100");
+// scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+// if(a >= 33 && b>=33 && c>=33 && d>=33 && e >= 33){
+//     printf("PASSED");
+// }
+// else
+// {
+//     printf("FAIL");
+// }
+
+char a = 'Z';
+
+if (a >= 65 && a <= 90 )
+{
+    printf("CAPITAL");
+}
+else{
+    printf("SMALL");    
+}
+
+}
