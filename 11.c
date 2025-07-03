69d53e6a37b4feb11d988a2e88ef5f27404aab98
Rohan singh negi <negirohan10ep@gmail.com>
2025-07-03 15:54:50 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+int main(){
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     printf("MySirG\n");
+    // }
+
+    // for (int i = 1; i < 11; i++)
+    // {
+    //     printf("Number : %d\n",i);
+    // }
+
+    
+    // for (int i = 10; i >= 1; i--)
+    // {
+    //     printf("Number : %d\n",i);
+    // }
+
+//    for (int i = 0; i <= 20 ; i++)
+//     {
+//         if(i%2 != 0){
+//         printf("Number : %d\n",i);
+//         }
+//     }   
+
+//    for (int i = 20; i >= 0 ; i--)
+//     {
+//         if(i%2 != 0){
+//         printf("Number : %d\n",i);
+//         }
+//     }
+
+//    for (int i = 1; i <= 20 ; i++)
+//     {
+//         if(i%2 == 0){
+//         printf("Number : %d\n",i);
+//         }
+//     }   
+
+//    for (int i = 20; i >= 1 ; i--)
+//     {
+//         if(i%2 == 0){
+//         printf("Number : %d\n",i);
+//         }
+//     }
+
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     printf("The square of %d is %d \n",i,i*i);
+    // }
+
+    // printf("\n");
+
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     printf("The cube of %d is %d \n",i, i*i*i);
+    // }
+    
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     printf("5 X %d = %d \n",i,i*5);
+    // }
+    
+
+    return 0;
+}
